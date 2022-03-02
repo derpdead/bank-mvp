15fbfbdddee4673db5605ceea2ea8d829d308447
Maciej Kaczorowski <46050724+derpdead@users.noreply.github.com>
2022-03-02 17:19:49 +0100

    Clean code

--- pages/api/consent-callback-get/index.ts
+++ pages/api/consent-callback-get/index.ts
@@ -26,9 +26,12 @@ export default async function handler(
             },
         }));
 
-        return res.status(200).end();
+        return res.status(200).json({
+            result: "OK",
+            reason: "Datos recibidos correctamente por el callback del cliente"
+        })
     } catch (e) {
         console.log(e);
-        return res.status(500).end();
+        return res.status(500);
     }
 }
